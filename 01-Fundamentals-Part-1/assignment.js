3fd2c56b0a868c01d7fc27ef91e29134e061b2ef
pformela_ <p.formela.030@studms.ug.edu.pl>
2022-03-10 20:44:05 +0100

    types, variables, constants

---
+++ 01-Fundamentals-Part-1/assignment.js
@@ -0,0 +1,17 @@
+const country = "Poland";
+const continent = "Europe";
+let population = 38000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(language);
+
+language = "polish";
+
+console.log(language);
